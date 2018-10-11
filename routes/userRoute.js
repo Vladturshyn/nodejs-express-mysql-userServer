dc7c15bcb5bfdf5eb02a2bcbdbe50eaaffc2de31
Vlad <vturshyn@gmail.com>
2018-10-11 18:09:38 +0300

    rest api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../userModel');
+const User = require('../model/userModel');
 
 router.get('/', (req, res)=>{
     User.find()
@@ -22,5 +22,32 @@ router.post('/', (req, res)=>{
         .then(data => res.json(data))
         .catch(err=> console.log(err));
 });
+
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndDelete(req.params.id)
+        .then(()=>{
+            res.json({data: true})
+        })
+        .catch((err)=>res.json(err));
+});
+
+router.put('/:id',(req, res) => {
+    User.findByIdAndUpdate( req.params.id ,{ name: req.body.name, email: req.body.email },{ new: true })
+        .then(user => {
+            if(err) {
+                res.status(400).json(err);
+            }
+            res.json({success: true});
+        })
+        .catch(err=> res.json(err))
+});
+router.get('/:id', (req,res)=>{
+    User.findById(req.params.id)
+        .then(user => {
+            if(user) res.json(user);
+            if(!user) res.json({err: 'not found'});
+        })
+        .catch(err=> res.json(err));
+})
 
 module.exports = router;
